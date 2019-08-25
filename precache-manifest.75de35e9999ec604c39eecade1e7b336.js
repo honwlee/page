9a97b80ac86cc56a7fe2e0d606d52c4aa79765bd
lihongwang <lhw@hudaokeji.com>
2019-08-25 17:20:08 +0800

    Updates

---
+++ precache-manifest.75de35e9999ec604c39eecade1e7b336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c29850abeb264f43a5b53b3355d9b6",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "f3f8737e79dec73b5a86",
+    "url": "/page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e7f1d391a4309583755d",
+    "url": "/page/static/js/2.cefbfde2.chunk.js"
+  },
+  {
+    "revision": "f3f8737e79dec73b5a86",
+    "url": "/page/static/js/main.8abe57d4.chunk.js"
+  },
+  {
+    "revision": "ee0361ff5400e193f8b2",
+    "url": "/page/static/js/runtime~main.069825cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/page/static/media/logo.5d5d9eef.svg"
+  }
+]);
